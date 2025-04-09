709dacbfcb476e557257f47aafe18fc94ec9149f
ihabhamed <ihab.hamed2000@gmail.com>
2025-04-09 10:14:12 +0200

    Update LoginForm.tsx

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect } from 'react';xxxx
+import React, { useState, useEffect } from 'react';
 import { useAuth } from './AuthContext';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
