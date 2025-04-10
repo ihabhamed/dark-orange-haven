fa448bcb8f04aafc0a6a188b675821167a791730
ihabhamed <ihab.hamed2000@gmail.com>
2025-04-10 14:07:22 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// main.tsx
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { AuthProvider } from './AuthProvider'; // تأكد من المسار الصحيح إذا غيرت مكانه
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+);
